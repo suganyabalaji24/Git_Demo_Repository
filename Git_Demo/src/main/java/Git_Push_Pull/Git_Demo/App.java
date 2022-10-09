dd0a0d58789b434a73dedc665be4d90760215b33
suganyabalaji24 <suganya_indian@yahoo.co.uk>
2022-10-09 19:31:51 +0530

    Git Demo commit message

---
+++ Git_Demo/src/main/java/Git_Push_Pull/Git_Demo/App.java
@@ -0,0 +1,13 @@
+package Git_Push_Pull.Git_Demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
